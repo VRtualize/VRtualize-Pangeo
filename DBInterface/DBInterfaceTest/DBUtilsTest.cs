4b6bae5f4982937b0770422b0f9610b42fd09f64
Chezka Gaddi <Chezka.Gaddi@mines.sdsmt.edu>
2019-11-11 20:42:03 -0700

    Tests for DBUtils

---
+++ DBInterface/DBInterfaceTest/DBUtilsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBInterface;
+using MySql.Data.MySqlClient;
+
+namespace DBInterfaceTest
+{
+    [TestClass]
+    public class DBUtilsTest
+    {
+        [TestMethod]
+        public void DBConnection_BadConnection()
+        {
+            // arrange connection with bad certificates
+            DBUtils util = new DBUtils("badhost", 0, "db", "user", "pass");
+
+            // act
+            bool connection_status = util.DBConnect();
+
+            // assert
+            Assert.IsFalse(connection_status);
+        }
+    }
+}
