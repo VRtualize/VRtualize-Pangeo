fa709a5765824cbfb00ef719b27eda4ad9e346dd
Michael Theesen <46730754+mtheesen@users.noreply.github.com>
2019-11-16 19:52:50 -0700

    For Loops running

--- Base Project/Assets/Scripts/GameHandler.cs
+++ Base Project/Assets/Scripts/GameHandler.cs
@@ -17,17 +17,22 @@ public static class Startup
         UsgsMapResources res = new UsgsMapResources();
         Cache cache = new Cache();
         cache.setMesh(res);
-        List<float> TestList = cache.getMesh();
-        //Debug.Log(TestList[1][1]);
+        List<float> ElevList = cache.getMesh();
         
         //Assign the GameObject material from the Resources folder
         Material mat = Resources.Load("Materials/Stylize_Grass", typeof(Material)) as Material;
-        
-        //Create empty vertices, uv, and triangles arrays and set their values
-        Vector3[] vertices = new Vector3[4];
-        Vector2[] uv = new Vector2[4];
-        int[] triangles = new int[6];
 
+        //Perform array calculations
+        int length = ElevList.Count;
+        int side = (int) Mathf.Sqrt(length);
+        int sqrPtCount = (int) (Mathf.Pow( (side - 1), 2) * 6);
+
+        //Create empty vertices, uv, and triangles arrays and set their values
+        Vector3[] vertices = fillVert(length, side, ElevList);
+        Vector2[] uv = fillUv(length, side);
+        int[] triangles = fillTri(sqrPtCount, side);
+
+        /*
         vertices[0] = new Vector3(0,1);
         vertices[1] = new Vector3(1,1);
         vertices[2] = new Vector3(0,0);
@@ -44,6 +49,7 @@ public static class Startup
         triangles[3] = 2;
         triangles[4] = 1;
         triangles[5] = 3;
+        */
 
         //Create a new Mesh to render
         Mesh mesh = new Mesh();
@@ -51,6 +57,8 @@ public static class Startup
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
 
         //Assign the Mesh to the rendering engine and transform into position
         GameObject Obj = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
@@ -61,6 +69,55 @@ public static class Startup
         Obj.GetComponent<MeshFilter>().mesh = mesh;
 
         Obj.GetComponent<MeshRenderer>().material = mat;
+
     }
 
+    static Vector3[] fillVert(int len, int side, List<float> ElevPts)
+    {
+        Vector3[] tmp = new Vector3[len];
+        int pos = 0;
+        for(int i = 0; i < side; i++)
+        {
+            //Debug.Log("i =" + i);
+            for(int j = 0; j < side; j++)
+            {
+                tmp[pos] = new Vector3(i, (side - j), (-1) * ElevPts[pos]);
+                pos++;
+            }
+        }
+        return tmp;
+    }
+
+    static Vector2[] fillUv(int len, int side)
+    {
+        Vector2[] tmp = new Vector2[len];
+        int pos = 0;
+        for (int i = 0; i < side; i++)
+        {
+            for (int j = 0; j < side; j++)
+            {
+                tmp[pos] = new Vector3(i, (side - j));
+                pos++;
+            }
+        }
+        return tmp;
+    }
+
+    static int[] fillTri(int ptCount, int side)
+    {
+        int[] tmp = new int[ptCount];
+        int pos = 0;
+        int sqrCount = ptCount / 6;
+        for (int i = 0; i < sqrCount; i++)
+        {
+            int sqrRow = (int) (i / (side - 1));
+            tmp[pos++] = i + sqrRow;
+            tmp[pos++] = i + 1 + sqrRow;
+            tmp[pos++] = i + side + sqrRow;
+            tmp[pos++] = i + side + sqrRow;
+            tmp[pos++] = i + 1 + sqrRow;
+            tmp[pos++] = i + side + 1 + sqrRow;
+        }
+        return tmp;
+    }
 }
