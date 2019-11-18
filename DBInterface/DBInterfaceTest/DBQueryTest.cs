ce7d6b82235bec36866d291c1fd77a387d59a9ca
Chezka Gaddi <Chezka.Gaddi@mines.sdsmt.edu>
2019-11-17 18:56:30 -0700

    Added DBInterface documentation.

---
+++ DBInterface/DBInterfaceTest/DBQueryTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBInterface;
+using MySql.Data.MySqlClient;
+
+namespace DBInterfaceTest
+{
+    [TestClass]
+    public class DBQueryTest
+    {
+        [TestMethod]
+        public void Query_GetMapChunk()
+        {
+            // arrange connection
+            DBUtils util = new DBUtils();
+            util.DBConnect();
+            MySqlConnection conn = util.GetDBConnection();
+
+            // act
+            MapData chunk = DBQuery.GetMapChunks(conn, -103.205985, 44.074386);
+
+            // assert
+            Assert.IsNotNull(chunk);
+        }
+
+        [TestMethod]
+        public void Query_GetMapChunk_NotInDatabase()
+        {
+            // arrange connection
+            DBUtils util = new DBUtils();
+            util.DBConnect();
+            MySqlConnection conn = util.GetDBConnection();
+
+            // act
+            MapData chunk = DBQuery.GetMapChunks(conn, 0, 0);
+
+            // assert
+            Assert.IsNull(chunk);
+        }
+    }
+}
---
+++ DBInterface/DBInterfaceTest/MapDataTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBInterface;
+
+namespace DBInterfaceTest
+{
+    [TestClass]
+    public class MapDataTest
+    {
+        MapData map;
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.map = new MapData(1, 10, 15, 10.001, 15.01, 100.02, "LSBFIRST");
+        }
+
+        [TestMethod]
+        public void Get_Latlongid()
+        {
+            int latlongid = map.GetLatLongId();
+            Assert.AreEqual(1, latlongid);
+        }
+
+        [TestMethod]
+        public void Get_NCols()
+        {
+            int ncols = map.GetNCols();
+            Assert.AreEqual(10, ncols);
+        }
+
+        [TestMethod]
+
+        public void Get_NRows()
+        {
+            int nrows = map.GetNRows();
+            Assert.AreEqual(15, nrows);
+        }
+
+        [TestMethod]
+
+        public void Get_Xllcorner()
+        {
+            double xllcorner = map.GetXllcorner();
+            Assert.AreEqual(10.001, xllcorner);
+        }
+
+        [TestMethod]
+
+        public void Get_Yllcorner()
+        {
+            double yllcorner = map.GetYllcorner();
+            Assert.AreEqual(15.01, yllcorner);
+        }
+
+        [TestMethod]
+
+        public void Get_Cellsize()
+        {
+            double cellsize = map.GetCellsize();
+            Assert.AreEqual(100.02, cellsize);
+        }
+
+        [TestMethod]
+
+        public void Get_Byteorder()
+        {
+            string byteorder = map.GetByteorder();
+            Assert.AreEqual("LSBFIRST", byteorder);
+        }
+    }
+}
