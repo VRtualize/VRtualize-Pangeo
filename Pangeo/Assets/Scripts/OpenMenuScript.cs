44fd25a5f162a06457b9ed3a9b23914d8cc0f3bd
Chezka Gaddi <chezkagaddi@gmail.com>
2019-12-01 16:58:19 -0700

    Menu button opens up the menu.

---
+++ Pangeo/Assets/Scripts/OpenMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Valve.VR;
+
+public class OpenMenuScript : MonoBehaviour
+{
+    public SteamVR_Action_Boolean OpenMenu;
+    public SteamVR_Input_Sources handType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Activating menu");
+
+        OpenMenu.AddOnStateUpListener(MenuInactive, handType);
+        OpenMenu.AddOnStateDownListener(MenuActive, handType);
+    }
+
+    public void MenuActive(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Activating menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void MenuInactive(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Deactivating menu");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
