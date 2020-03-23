08074df39547e32116616b95968f5fe704880407
Michael Theesen <46730754+mtheesen@users.noreply.github.com>
2020-03-23 17:51:02 -0600

    Location data can be toggled off or on with keyboard input
    Currently the location data can be turned on or off by pressing E key on the keyboard. The process is a little buggy as multiple frames can render during the time of the button press, causing the menu to glitch off or on too fast. As of now, no actual data is connected to panel, need global variables to access for the information.

---
+++ Pangeo/Assets/Scripts/MenuScripts/LocationDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationDisplay : MonoBehaviour
+{
+    Text display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = GetComponent<Text>();
+
+        display.text = "Latitude:" +
+            "\nLongitude:";
+    }
+
+    private void Update()
+    {
+        display = GetComponent<Text>();
+
+        display.text = "Latitude: 0.000000000" +
+            "\nLongitude: 0.00000000";
+    }
+}
---
+++ Pangeo/Assets/Scripts/MenuScripts/ToggleLocation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLocation : MonoBehaviour
+{
+    GameObject LocDisplay;
+    bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocDisplay = GameObject.Find("LocationCanvas");
+        active = false;
+        LocDisplay.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            if(active)
+            {
+                active = false;
+                LocDisplay.SetActive(false);
+            }
+            else
+            {
+                active = true;
+                LocDisplay.SetActive(true);
+            }
+        }
+    }
+}
