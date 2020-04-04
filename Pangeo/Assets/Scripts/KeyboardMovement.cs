57a25ba43e4878d74fe880917bf6cf187b137f24
weiluntong <weiluntong.inbox@gmail.com>
2020-04-04 14:28:04 -0600

    Changed up key, added Globals.position, reduced speed

--- Pangeo/Assets/Scripts/KeyboardMovement.cs
+++ Pangeo/Assets/Scripts/KeyboardMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +11,7 @@ using UnityEngine;
 /// </summary>
 public class KeyboardMovement : MonoBehaviour
 {
-    float speed = 200.0f;       // Default speed
+    float speed = 20.0f;       // Default speed
     float sensitivity = 0.25f;  // Mouse sensitivity
     private Vector3 centerMouse = new Vector3(255, 255, 255);   // Place mouse in the middle of the screen rather than at the top
     private float totalRun = 1.0f;
@@ -46,12 +46,13 @@ public class KeyboardMovement : MonoBehaviour
         newPosition.x = transform.position.x;
         newPosition.z = transform.position.z;
 
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftShift))
         {
             newPosition.y = transform.position.y;
         }
         
         transform.position = newPosition;
+        Globals.position = newPosition;
     }
    
     /// <summary>
@@ -78,7 +79,7 @@ public class KeyboardMovement : MonoBehaviour
         {
             p_Velocity += new Vector3(1, 0, 0);
         }
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             p_Velocity += new Vector3(0, -1, 0);
         }
